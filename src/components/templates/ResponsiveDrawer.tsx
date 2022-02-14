8e1027c7e2b23a8d22cb15885d5cea105ee68aff
ogasawara <ogaru@ogasawayuunoAir.elecom>
2022-02-14 20:32:26 +0900

    sideBarを分割

--- src/components/templates/ResponsiveDrawer.tsx
+++ src/components/templates/ResponsiveDrawer.tsx
@@ -7,61 +7,17 @@ import { DefaultDrawer } from '@/components/molecules/responsiveDrawer/DefaultDr
 import { Header } from '@/components/organisms/responsiveDrawer/AppBar';
 import { constant } from '@/ts/modules/config';
 import { Props } from '@/ts/modules/interfaces';
+import { SideBar } from '../organisms/responsiveDrawer/SideBar';
 
 export const ResponsiveDrawer = (props: Props) => {
   const drawerWidth = constant.drawerWidth;
   const { window, children } = props;
   const [mobileOpen, setMobileOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
-
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <Header />
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': {
-              boxSizing: 'border-box',
-              width: drawerWidth
-            }
-          }}
-        >
-          <DefaultDrawer />
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': {
-              boxSizing: 'border-box',
-              width: drawerWidth
-            }
-          }}
-          open
-        >
-          <DefaultDrawer />
-        </Drawer>
-      </Box>
+      <SideBar window={window} />
       <Box
         component="main"
         sx={{
