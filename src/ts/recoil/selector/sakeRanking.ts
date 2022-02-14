cd51ed172c2f05e4390ba771bcb36c13b77d5346
ogasawara <ogaru@ogasawayuunoAir.elecom>
2022-02-14 15:47:05 +0900

    fetchAPIのレスポンスに型を付与

--- src/ts/recoil/selector/sakeRanking.ts
+++ src/ts/recoil/selector/sakeRanking.ts
@@ -1,9 +1,9 @@
-import { selector, DefaultValue, selectorFamily } from 'recoil';
+import { selectorFamily } from 'recoil';
 import {
   sakeRankingParamState,
   sakeRankingState
 } from '@/ts/recoil/atom/sakeRanking';
-import { FetchAPI, SakeRanking } from '@/ts/modules/interfaces';
+import { SakeRanking } from '@/ts/modules/interfaces';
 import { fetchAPI } from '@/ts/modules/fetchAPI';
 import { config } from '@/ts/modules/config';
 
@@ -14,13 +14,16 @@ export const sakeRankingSelector = selectorFamily({
     async ({ get }) => {
       const sakeRanking = get(sakeRankingState);
       const sakeRankingParam = get(sakeRankingParamState);
-      const sakeParam =
+      const sakeParam: string =
         sakeRankingParam.length === 0 ? '' : sakeRankingParam.join('&');
       if (sakeRanking && param === sakeParam) {
         return sakeRanking;
       } else {
         try {
-          const response = fetchAPI(config.sakeAPI.basePath.ranking, param);
+          const response: Promise<SakeRanking | null> = fetchAPI(
+            config.sakeAPI.basePath.ranking,
+            param
+          );
           return response;
         } catch (error: any) {
           throw new Error(error);
